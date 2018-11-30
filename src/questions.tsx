af0f333b91034ae2911f8b81e8915a80ca6b7a3b
Brian Birtles <birtles@gmail.com>
2018-11-30 19:01:32 +0900

    Add third question

--- src/questions.tsx
+++ src/questions.tsx
@@ -14,23 +14,43 @@ export const questions = [
       <img src="/img/toyosu.jpg" />
     </div>
     <p className="hint">
-      <strong>ヒント：</strong>すべての駅は今日のメンバーの誰かの最寄り駅です。
+      <strong>ヒント：</strong>
+      すべての駅は今日のメンバーの誰かの最寄り駅です。
+    </p>
+  </>,
+  <>
+    <p>世界中様々なクリスマス料理がありますが、これはなんでしょうか？</p>
+    <div className="photos">
+      <img src="/img/cakewich.jpg" />
+    </div>
+    <p>もしケーキがサンドウィッチと結婚したら、どんな子供が生まれる？🍰🥪💒</p>
+    <p className="hint">
+      <strong>ヒント：</strong>
+      横浜に在住する優しい子が知っている。
     </p>
   </>,
   <>
     <p>
-      世界中様々なクリスマス料理がありますが、これはなんでしょうか？
+      クリスマスにプレゼント交換する人がいます🎁
+      <br />
+      それはイエスが生まれたとき、アジアから博士たちが来て、イエスにプレゼントをあげたからです。
     </p>
     <div className="photos">
-      <img src="/img/cakewich.jpg" />
+      <img src="/img/wisemen.jpg" />
     </div>
-    <p>
-      もしケーキがサンドウィッチと結婚したら、どんな子供が生まれる？🍰🥪💒
-    </p>
+    <p>博士たちがあげたプレゼント一つを教えてください。</p>
     <p className="hint">
-      <strong>ヒント：</strong>横浜に在住する優しい子が知っている。
+      <strong>ヒント：</strong>
+      少なくとも３種類のプレゼントありました。
+      クリスマスに教会に行ったことのある人は知っているかなぁ。 （または
+      <a
+        href="http://bible.salterrae.net/kougo/html/matthew.html#2"
+        target="_blank"
+      >
+        聖書
+      </a>
+      で調べられる。）
     </p>
   </>,
-  <div>Question 3</div>,
   <div>Question 4</div>,
 ];
